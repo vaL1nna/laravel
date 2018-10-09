d8a7c86ce8b4112d81b399a35914b54ef546301a
wengxiaotong <345561446@qq.com>
2018-10-09 23:13:43 +0800

    ssss

--- app/Http/Controllers/Admin/ApplicationController.php
+++ app/Http/Controllers/Admin/ApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Nav;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -32,8 +33,22 @@ class ApplicationController extends Controller
     {
         if ($request->isMethod('post')){
             //接受参数
-            $params = $request->only('menu_id', 'order_id', 'nav_name', 'nav_content', 'keyword', 'title', 'description', 'url', 'is_show', 'nav_attribute1', 'nav_attribute2', 'nav_attribute3', 'nav_attribute4', 'nav_attribute5', 'nav_attribute6', 'nav_attribute7', 'nav_attribute8', 'nav_attribute9', 'nav_attribute10');
-
+            $params = $request->only('nav_name', 'position', 'keyword', 'title', 'description', 'url', 'nav_content');
+            if ($request->hasFile('nav_image')) {
+                $image = $request->file('nav_image');
+                if ($image->isValid()) {
+                    $ext = $image->getClientOriginalExtension();
+                    $fileTypes = array('gif','png','jpg','jpeg');
+                    if (!in_array($ext, $fileTypes)) {
+                        return response()->json(['error' => '图片格式不正确']);
+                    }
+                    $path = $image->store('public');
+                    $params['nav_image'] = str_replace('public', '/storage', $path);
+                }
+            }
+            //获取父类id
+            $parent_id = Nav::where('parent_id', '0')->where('nav_type', '3')->first()->id;
+            $params['parent_id'] = $parent_id;
             $data = Nav::create($params);
             $data->order_id = $data->id;
             $result = $data->save();
@@ -41,12 +56,8 @@ class ApplicationController extends Controller
                 return response()->json($result);
             }
         }
-        //获取所有分类信息
-        $menu = Nav::where('type_id', '2')->where('parent_id', '!=', '0')->orderBy('order_id')->get();
 
-        return view('Admin.nav.add', ['menu' => $menu]);
-
-
+        return view('Admin.nav.add');
     }
 
     public function edit(Request $request)
@@ -57,7 +68,7 @@ class ApplicationController extends Controller
         if ($request->isMethod('post')) {
             //接受参数
             $id = $request->id;
-            $params = $request->only('menu_id', 'order_id', 'nav_name', 'nav_content', 'keyword', 'title', 'description', 'url', 'nav_image', 'nav_file', 'is_show', 'nav_attribute1', 'nav_attribute2', 'nav_attribute3', 'nav_attribute4', 'nav_attribute5', 'nav_attribute6', 'nav_attribute7', 'nav_attribute8', 'nav_attribute9', 'nav_attribute10');
+            $params = $request->only('nav_name', 'position', 'keyword', 'title', 'description', 'url', 'nav_content');
 
             if ($request->hasFile('nav_image')) {
                 $image = $request->file('nav_image');
@@ -74,31 +85,11 @@ class ApplicationController extends Controller
                 $params['nav_image'] = $info['nav_image'];
             }
 
-            if ($request->hasFile('nav_file')) {
-                $file = $request->file('nav_file');
-                if ($file->isValid()) {
-                    /*$fileName = $file->getClientOriginalName();
-                    $fileName = explode('.', $fileName)[0] . '_' . date('ymd');*/
-                    $ext = $file->getClientOriginalExtension();
-                    if ($ext != 'pdf') {
-                        return response()->json(['error' => '上传的pdf格式不正确']);
-                    }
-                    $path = $file->store('public');
-                    $params['nav_file'] = str_replace('public', '/storage', $path);
-                }
-            }else{
-                $params['nav_file'] = $info['nav_file'];
-            }
-
             $nav = Nav::find($id)->update($params);
             return response()->json($nav);
         }
 
-        //获取所有分类信息
-        $menu = Nav::where('type_id', '2')->where('parent_id', '!=', '0')->orderBy('order_id')->get();
-
-
-        return view('Admin.nav.edit', ['menu' => $menu, 'info' => $info]);
+        return view('Admin.nav.edit', ['info' => $info]);
     }
 
     public function del(Request $request)
